8a1e8ea8e87453f6d3807a84d1b917a7d5b239b3
gabesullice <gabriel@sullice.com>
2014-10-21 12:28:26 -0600

    Add declarative example.

--- 01_declarative.php
+++ 01_declarative.php
@@ -1,2 +1,14 @@
-test
-test
+<?php
+
+echo "<ul>";
+echo "  <li class=\"first\">First Content</li>";
+echo "  <li>Some Content</li>";
+echo "  <li>Some Content</li>";
+echo "  <li>Some Content</li>";
+echo "  <li>Some Content</li>";
+echo "  <li>Some Content</li>";
+echo "  <li>Some Content</li>";
+echo "  <li class=\"last\">Last Content</li>";
+echo "</ul>";
+
+?>
