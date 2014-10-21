80b5e86f8e0f4120d9855b8bf68f3d80f2025658
gabesullice <gabriel@sullice.com>
2014-10-21 12:39:24 -0600

    Add procedural examples and their evolution.

--- 02_procedural.php
+++ 02_procedural.php
@@ -1,2 +1,27 @@
-test
-test
+<?php
+
+echo "<ul>";
+echo "  <li class=\"first\">First Content</li>";
+
+
+$i = 0;
+
+echoline:
+echo "  <li>Some Content</li>";
+$i = $i + 1;
+
+if ($i < 6) goto echoline;
+
+
+echo "  <li class=\"last\">Last Content</li>";
+echo "</ul>";
+
+echo "<ul>";
+echo "  <li class=\"first\">First Content</li>";
+for ($i = 0; $i < 6; $i++) {
+  echo "  <li>Some Content</li>";
+}
+echo "  <li class=\"last\">Last Content</li>";
+echo "</ul>";
+
+?>
