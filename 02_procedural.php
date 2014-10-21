c11ca6751e92f3e578ef181daf29d73a3ba26feb
gabesullice <gabriel@sullice.com>
2014-10-21 16:18:09 -0600

    Combine 'functional' into procedural example.
    This helps avoid confusion w/ funtional programming, like w/ Haskell.

--- 02_procedural.php
+++ 02_procedural.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * ============================================================================
+ * Programming before loops
+ * ============================================================================
+ */
+
 echo "<ul>";
 echo "  <li class=\"first\">First Content</li>";
 
@@ -16,12 +22,42 @@ if ($i < 6) goto echoline;
 echo "  <li class=\"last\">Last Content</li>";
 echo "</ul>";
 
+/**
+ * ============================================================================
+ * Programming with loops, w/o functions
+ * ============================================================================
+ */
+
 echo "<ul>";
 echo "  <li class=\"first\">First Content</li>";
 for ($i = 0; $i < 6; $i++) {
   echo "  <li>Some Content</li>";
 }
 echo "  <li class=\"last\">Last Content</li>";
+echo "</ul>";
+
+/**
+ * ============================================================================
+ * Programming with functions
+ * ============================================================================
+ */
+
+function echo_li($content, $class = FALSE) {
+  $li = ($class) ? "<li class=\"" . $class . "\">" : "<li>";
+  $li .= $content . "</li>";
+  echo $li;
+}
+
+echo "<ul>";
+
+echo_li('First Content', 'first');
+
+for ($i = 0; $i < 6; $i++) {
+  echo_li('Some Content');
+}
+
+echo_li('Last Content', 'last');
+
 echo "</ul>";
 
 ?>
