1d4a78a0485d5fcd10a0b40864fd2852fadc80dc
gabesullice <gabriel@sullice.com>
2014-10-21 15:49:22 -0600

    Add first iteration of simple OO list class.

--- 04_objectoriented.php
+++ 04_objectoriented.php
@@ -1,1 +1,47 @@
+<?php
 
+class listClass {
+  var $name;
+  var $items;
+
+  function addItem($content, $class = FALSE) {
+    $this->items[] = array(
+      'content' => $content,
+      'class' => $class,
+    );
+  }
+
+  function printList() {
+    echo "<ul>";
+    echo "<h3>" . $this->name . "</h3>";
+
+    foreach ($this->items as $item) {
+      $li = ($item['class']) ? "<li class=\"" . $item['class'] . "\">" : "<li>";
+      $li .= $item['content'] . "</li>";
+      echo $li;
+    }
+
+    echo "</ul>";
+  }
+}
+
+function makeList($name) {
+  $list = new listClass;
+  $list->name = $name;
+
+  $list->addItem('First Content', 'first');
+  for ($i = 0; $i < 6; $i++) {
+    $list->addItem('Some Content');
+  }
+  $list->addItem('Last Content', 'last');
+
+  return $list;
+}
+
+$firstList = makeList('My first list object');
+$secondList = makeList('My second list object');
+
+$firstList->printList();
+$secondList->printList();
+
+?>
