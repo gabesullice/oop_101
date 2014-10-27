8b86f43dfd8cbaf90981d376907fc62f9872e86b
gabesullice <gabriel@sullice.com>
2014-10-27 13:23:14 -0600

    Add comments to 02.

--- 02_procedural.php
+++ 02_procedural.php
@@ -1,6 +1,39 @@
 <?php
 
 /**
+ * With the introduction of compilers and interpreters languages were able to
+ * introduce basic concepts like variables and simple conditionals.
+ *
+ * Loops were quickly added.
+ *
+ * The strict separation of data and behavior remained, but they were still
+ * necessarily coupled.
+ * 
+ * The function allowed complex behaviors to be written somewhat isolated from
+ * input data and reused in many places. This greatly aided in the maintenance
+ * of a program as one could write programs such that common behaviors were
+ * maintained in one place and updating the structure of your data or coming up
+ * with a more performant way of processing it no longer forced you to change
+ * your code across 100s and 1000s of different lines. Common programming best
+ * practices like Don't Repeat Yourself (DRY) and the Single Responsibilty
+ * Principle (SRP) were now possible.
+ *
+ * Still, because data and behavior remained separate, one had to sacrifice
+ * performance for maintainability and flexibility, even if only trivially.
+ * 
+ * For example, before one could abstract their operations out into funtions,
+ * every operation performed on input data could be meticulously tailored to
+ * that data. Every loop and conditional could be reasonbly certain of what
+ * kind and structure of data it would be working with. In a fucntion that
+ * might be reused across a program it now would need to pause and reflect on
+ * the data. Where before every print operation in one place could assume every
+ * string to print was less than 80 characters and in another it could assume a
+ * newline was needed, a central print function would need to assess the length
+ * of every line and conditionally insert a newline as necessary. This
+ * additional work would slow the process down.
+ */
+
+/**
  * ============================================================================
  * Programming before loops
  * ============================================================================
