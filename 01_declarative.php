50756fc2848c54cfbf834687766a27653fd03abd
gabesullice <gabriel@sullice.com>
2014-10-27 12:39:46 -0600

    Add comments to 01.

--- 01_declarative.php
+++ 01_declarative.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * The first declaritive programs executed line by line performing predefined
+ * actions made available by the programming language, and before that embedded
+ * into the computer hardware. There were no control structures like if, then,
+ * for, or while, etc.
+ * 
+ * Behavior and function are strictly separate. Calling a behavior and then
+ * explicitly providing data to that behavior.
+ */
+
 echo "<ul>";
 echo "  <li class=\"first\">First Content</li>";
 echo "  <li>Some Content</li>";
