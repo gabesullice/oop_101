ff64d56a93aea7009aebc1541d39091332650e0e
gabesullice <gabriel@sullice.com>
2014-11-04 09:07:34 -0700

    Update public, protected, private example.

--- 07.pubprivprot.php
+++ 07.pubprivprot.php
@@ -5,89 +5,101 @@ class element {
   protected $content; // array of content, either string or element.
   protected $attr; // array of arrays, keyed by attribute name (e.g. array('class' => array('one', 'two'))
 
-  function __construct($type, $content = NULL, $attr = NULL) {
+  public function __construct($type, $content = NULL, $attr = NULL) {
     $this->type = $type;
-    $this->content = $content;
+    $this->addContent($content);
     $this->attr = $attr;
   }
 
-  function printElem() {
-    echo "<" . $this->type . " ";
+  public function addContent($content) {
+    if (is_array($content)) {
+      foreach ($content as $item) {
+        $this->content[] = $item;
+      }
+    }
+    elseif (is_a($content, 'element') || is_string($content)) {
+      $this->content[] = $content;
+    }
+  }
 
+  public function render() {
+    $type = $this->type;
+    $attr = $this->renderAttr();
+    $content = $this->renderContent();
+
+    return sprintf("<%s%s>%s</%s>\n",
+      $type,
+      $attr,
+      $content,
+      $type
+    );
+  }
+
+  protected function renderContent() {
+    $content = "";
+    foreach ($this->content as $item) {
+      if (is_object($item) && is_a($item, 'element')) {
+        $content .= $item->render();
+      }
+      elseif (is_string($item)) {
+        $content .= $item;
+      }
+    }
+    return $content;
+  }
+
+  protected function renderAttr() {
+    $content = "";
     if (isset($this->attr)) {
       foreach ($this->attr as $name => $values) {
-        echo $name . "=\"" . implode(' ', $values)  . "\" ";
+        $content .= sprintf(" %s=\"%s\" ",
+          $name,
+          implode(' ', $values)
+        );
       }
     }
-
-    echo ">";
-
-    if (is_array($this->content)) {
-      foreach ($this->content as $line) {
-        if (is_object($line) && is_a($line, 'element')) {
-          $line->printElem();
-        }
-        elseif (is_string($line)) {
-          echo $line;
-        }
-      }
-    }
-    elseif (is_string($this->content)) {
-      echo $this->content;
-    }
-
-    echo "</" . $this->type . ">\n";
+    return $content;
   }
+
 }
 
 class li extends element {
-  function __construct($content = NULL, $attr = NULL) {
-    $this->type = 'li';
-    $this->content = $content;
-    $this->attr = $attr;
+  public function __construct($content = NULL, $attr = NULL) {
+    parent::__construct('li', $content, $attr);
   }
 }
 
 class ul extends element {
   public $name;
 
-  function __construct($name, $attr = NULL) {
+  public function __construct($name, $attr = NULL) {
     $this->name = $name;
-    $this->type = 'ul';
-    $this->attr = $attr;
-    $this->content[] = new element('h3', $this->name);
-
+    parent::__construct('ul', array(new element('h3', $this->name)), $attr);
     $this->makeExample();
-
-    return $list;
-  }
-
-  function addItem($item) {
-    $this->content[] = $item;
   }
 
   private function makeExample() {
     $item = new li('First Content', array('class' => array('first')));
-    $this->addItem($item);
+    $this->addContent($item);
 
     for ($i = 0; $i < 6; $i++) {
       $item = new li('Some Content');
-      $this->addItem($item);
+      $this->addContent($item);
     }
 
     $item = new li('Last Content', array('class' => array('last')));
-    $this->addItem($item);
+    $this->addContent($item);
   }
 }
 
 $firstList = new ul('My first list object');
 $secondList = new ul('My second list object');
 
-$firstList->printElem();
-$secondList->printElem();
+print $firstList->render();
+print $secondList->render();
 
 $firstLIst->name = 'My first list with a new name';
-echo $firstLIst->name;
-$firstList->printElem(); // Why does this not print the new name?
+print $firstLIst->name;
+print $firstList->render(); // Why does this not print the new name?
 
 ?>
